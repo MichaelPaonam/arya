972ae95b270e8bdd61c511d10d1d9019516edf03
Michael Paonam <mazdaswift@gmail.com>
2026-05-01 18:07:17 +0530

    layout modification

--- packages/frontend/src/app/(marketing)/layout.tsx
+++ packages/frontend/src/app/(marketing)/layout.tsx
@@ -1,11 +1,7 @@
-"use client";
-
-import { ThemeProvider } from "@/components/theme-provider";
-
 export default function MarketingLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return <>{children}</>;
 }
